af98b6c66212640950434478b059d4962d216e2d
LazarBojanic <bojanic.lazar50@gmail.com>
2026-01-04 19:05:14 +0100

    misc

--- src/main/java/com/lazar/absolutecinema/Main.java
+++ src/main/java/com/lazar/absolutecinema/Main.java
@@ -1,16 +1,15 @@
 package com.lazar.absolutecinema;
 
 import com.lazar.absolutecinema.core.App;
-import com.lazar.absolutecinema.parser.ast.LType;
 
 class Main {
-	public static void main(String[] args) {
-		try {
-			App app = new App(args);
-			app.run();
-		}
-		catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-	}
+
+    public static void main(String[] args) {
+        try {
+            App app = new App(args);
+            app.run();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
